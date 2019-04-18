274c7dc99a3f08d53d854f96729b3346933c1aae
zhuming <zhuming@autoai.com>
2019-04-18 20:46:40 +0800

    canvas

---
+++ 1_ui/0_ui_core/2_paint/2_canvas/app/src/main/java/com/soul/canvas/TransformView.java
@@ -0,0 +1,12 @@
+package com.soul.canvas;
+
+/**
+ * Description:
+ * Author: 祝明
+ * CreateDate: 2019/4/18 下午5:35
+ * UpdateUser:
+ * UpdateDate: 2019/4/18 下午5:35
+ * UpdateRemark:
+ */
+public class TransformView {
+}
